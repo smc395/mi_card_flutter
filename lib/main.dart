de3658c17235b5acea5332a309d0852b7a73de54
Sung Yan Micah Chao <symchao@gmail.com>
2020-04-07 22:17:01 -1000

    Code for the Flutter layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
+/* Flutter Layout Challenge Code */
 void main() {
   runApp(
-    MaterialApp(
+    LayoutChallenge(),
+  );
+}
+
+class LayoutChallenge extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Container(width: 100.0, color: Colors.blue),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
