2898756434691b3f00da1cd4cbb21eb07373f35b
Sung Yan Micah Chao <symchao@gmail.com>
2020-04-09 21:29:03 -1000

    Completed Mi Business Card project code

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,109 @@ import 'package:flutter/material.dart';
 /* Flutter Layout Challenge Code */
 void main() {
   runApp(
-    LayoutChallenge(),
+    MiCard(),
+    //LayoutChallenge(),
   );
+}
+
+class MiCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 55.0,
+                  backgroundImage: AssetImage('images/headshot.jpg'),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Micah Chao',
+                  style: TextStyle(
+                      fontFamily: 'Share',
+                      fontSize: 35.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'BI & Healthcare Analytics Analyst',
+                  style: TextStyle(
+                    fontFamily: 'Share',
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'The Queens Health Systems',
+                  style: TextStyle(
+                    fontFamily: 'Share',
+                    fontSize: 17.0,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+                Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.mail_outline,
+                      color: Colors.grey,
+                    ),
+                    title: Text(
+                      'email@gmail.com',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 18.0,
+                        fontFamily: 'Share',
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.code,
+                      color: Colors.grey,
+                    ),
+                    title: Text(
+                      'github.com/smc395',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 18.0,
+                        fontFamily: 'Share',
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class LayoutChallenge extends StatelessWidget {
